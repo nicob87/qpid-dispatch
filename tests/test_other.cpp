9b4bcae94667a52d24922260b67ed606c49e64c8
Nicolas Brignone <nmbrignone@gmail.com>
2020-02-12 18:56:48 -0300

    completed migration

--- tests/test_other.cpp
+++ tests/test_other.cpp
@@ -1,24 +1,5 @@
-/*
- * =====================================================================================
- *
- *       Filename:  test_other.cpp
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  02/04/2020 10:08:10 AM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (), 
- *   Organization:  
- *
- * =====================================================================================
- */
+#include "qdr_doctest.h"
 
-#include <gtest/gtest.h>
-
-TEST(test_other, thing) {
-    ASSERT_EQ(8, 8);
+TEST_CASE("test_other_thing") {
+    CHECK(8 == 8);
 }
-
