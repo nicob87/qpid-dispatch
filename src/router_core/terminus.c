4c0b908163a5517340202403490e22e74f69967d
Nicolas Brignone <nmbrignone@gmail.com>
2020-02-12 17:19:53 -0300

    updates safe_vsprintf to be equal to the pr version

--- src/router_core/terminus.c
+++ src/router_core/terminus.c
@@ -91,28 +91,28 @@ __attribute__((weak)) int _vsnprintf(char *str, size_t size, const char *format,
 // qdr_terminus_format()
 // not static to be used  unit-tested
 size_t safe_snprintf(char *str, size_t size, const char *format, ...) {
-    // max size allowed should be max possible int (since printf reutrns an
-    // int)
+    // max size allowed must be INT_MAX (since vsnprintf reutrns an int)
     if (size == 0 || size > INT_MAX) {
+        //TODO log a warning somewhere?
         return 0;
     }
-    size_t max_possible_return_value = size - 1;
+    int max_possible_return_value = (int)(size - 1);
     va_list ap;
     va_start(ap, format);
     int rc = _vsnprintf(str, size, format, ap);
     va_end(ap);
 
     if (rc < 0) { // parsing error!
-        return 0; // do we want to do this?
+        //TODO log a warning somewhere?
+        return 0;
     }
 
-    if (rc > max_possible_return_value) { // comparing int with size_t? why?
-        return max_possible_return_value; // return actual # of bytes
-                                          // written (excluding null)
+    if (rc > max_possible_return_value) {
+        //TODO log a warning somewhere?
+        rc = max_possible_return_value;
     }
-    return rc;
+    return (size_t)rc;
 }
-
 
 void qdr_terminus_format(qdr_terminus_t *term, char *output, size_t *size)
 {
